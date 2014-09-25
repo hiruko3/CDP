2d7aa03042c3c6a0e8523afd04d4b2fdd88ea54e
sjolly33 <fsjolly@yahoo.fr>
2014-09-25 18:59:49 +0200

    creation class Atelier

---
+++ src/resources/Atelier.php
@@ -0,0 +1,18 @@
+<?php
+class Atelier
+{
+	private $_id;
+	private $_nom;
+	private $_theme;
+	private $_type;
+	private $_discipline;
+	private $_resume;
+	private $_duree;
+	private $_capacite;
+	private $_inscription;
+	private $_laboratoire;
+	private $_adresse;
+	private $_zone;
+	private $_remarque;
+}
+?>
