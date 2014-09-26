2ca33c8426cd6476b9979283afc7bc18e51672d6
hiruko3 <romain3.verger3@gmail.com>
2014-09-26 14:55:10 +0200

    index.php

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<?php
+
+//On démarre la session
+session_start();
+
+ 
+//On inclut le contrôleur s'il existe et s'il est spécifié
+if (!empty($_GET['page']) && is_file('controleurs/'.$_GET['page'].'.php'))
+{
+        include 'controllers/'.$_GET['page'].'.php';
+}
+else
+{
+        include 'controllers/accueil.php';
+}
+ 
+ 
