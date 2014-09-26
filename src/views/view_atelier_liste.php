e00cf5a5c119648dd662867e27885b5789afb5d4
sjolly33 <fsjolly@yahoo.fr>
2014-09-26 21:56:04 +0200

    vue liste

---
+++ src/views/view_atelier_liste.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
+	<head>
+<!--    <link rel = "stylesheet" href = "" /> -->
+        <meta charset="utf-8" />
+        <title>CNRS_Website</title>
+	</head>
+	<body>
+        <h1>Liste des ateliers</h1>
+		<table>
+			<tr>
+				<th>Atelier</th>
+				<th>Th&egrave;me</th>
+				<th>Type</th>
+				<th>Discipline</th>
+			</tr>
+			<?php			
+				foreach($ateliers as $a)
+				{
+					echo "<tr>";
+						echo "<td><a href = controller_atelier_affichage.php?id=" . $a->get_id() . ">" . $a->get_nom() . "</a></td>";
+						echo "<td>" . $a->get_theme() . "</td>";
+						echo "<td>" . $a->get_type() . "</td>";
+						echo "<td>" . $a->get_discipline() . "</td>";
+					echo "</tr>";
+
+				/*<b> Thème : <?php echo $atelier['atelier_theme']; ?></b>
+				<em>Type : <?php echo $atelier['atelier_type']; ?></em>
+				<em>Discipline : <?php echo $atelier['atelier_discipline']; ?></em>
+				<em>Capacité : <?php echo $atelier['atelier_capacite']; ?></em>*/
+				}
+			?>
+		</table>
+	</body>
+</html>
