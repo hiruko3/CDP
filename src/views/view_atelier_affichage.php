a2f31592c7be4482d99d90ac8e9821fa0a7d2b61
Victor <victor.Dupin.pro@gmail.com>
2014-10-10 13:04:06 +0200

    #32 Creation_atelier_test

--- src/views/view_atelier_affichage.php
+++ src/views/view_atelier_affichage.php
@@ -20,22 +20,22 @@
 		<div class="atelier_display">
 			<h2>
 				<div class = "nom_atelier"><?php echo $atelier['atelier_nom']; ?></div>
-				<b> Thème : <?php echo $atelier['atelier_theme']; ?></b>
-				<em>Type : <?php echo $atelier['atelier_type']; ?></em>
-				<em>Discipline : <?php echo $atelier['atelier_discipline']; ?></em>
-				<em>Capacité : <?php echo $atelier['atelier_capacite']; ?></em>
+				<b id="theme">  Thème : <?php echo $atelier['atelier_theme']; ?></b>
+				<em id="type"> Type : <?php echo $atelier['atelier_type']; ?></em>
+				<em id="discipline"> Discipline : <?php echo $atelier['atelier_discipline']; ?></em>
+				<em id="capacite"> Capacité : <?php echo $atelier['atelier_capacite']; ?></em>
 			</h2>
 			<h3>
-				Durée : <?php echo $atelier['atelier_duree']; ?>
-				Adresse : <?php echo $atelier['atelier_adresse']; ?>
-				Zone : <?php echo $atelier['atelier_zone']; ?>
+				<em id="duree"> Durée : <?php echo $atelier['atelier_duree']; ?></em>
+				<em id="adresse"> Adresse : <?php echo $atelier['atelier_adresse']; ?></em>
+				<em id="zone"> Zone : <?php echo $atelier['atelier_zone']; ?></em>
 			</h3>
 			<p>
-				Résumé de l'atelier : <?php echo $atelier['atelier_resume']; ?>
+				<em id="resume"> Résumé de l'atelier : <?php echo $atelier['atelier_resume']; ?></em>
 			</p>
 			<br/>
 			<p>
-				Remarque concernant l'atelier : <?php $atelier['atelier_remarque']?>
+				<em id="remarque"> Remarque concernant l'atelier : <?php $atelier['atelier_remarque']?></em>
 			</p>
 		</div>
 		<?php
